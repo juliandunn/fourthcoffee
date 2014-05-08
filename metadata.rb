c585d4ec31308bb316c37dddac6a80bf95480cd7
Julian C. Dunn <jdunn@getchef.com>
2014-05-08 17:26:29 -0400

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'fourthcoffee'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'jdunn@getchef.com'
+license          'Apache 2.0'
+description      'Installs and configures the Fourth Coffee demonstration website'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'dsc' 
---
+++ recipes/default.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: fourthcoffee
+# Recipe:: default
+#
+# Copyright 2014, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install DSC Resource Kit, Wave 3
+# This gets us xWebsite, xWebAdministration, and so on
+include_recipe 'dsc::getdscresourcekit'
+
+include_dsc 'WindowsFeature'
+#include_dsc 'xWebAdministration'
+include_dsc 'xWebsite'
+
+dsc_windowsfeature 'iis' do
+  dsc_name 'Web-Server'
+  dsc_ensure 'Present'
+end
+
+dsc_windowsfeature 'dotnet45' do
+  dsc_name 'Web-Asp-Net45'
+  dsc_ensure 'Present'
+end
+
+dsc_xwebsite 'DefaultSite' do
+  dsc_name 'Default Web Site'
+  dsc_ensure 'Stopped'
+end
+
+remote_directory node['fourthcoffee']['install_path'] do
+  source 'fourthcoffee'
+  # might need rights here
+  action :create
+end
+
+dsc_xwebsite 'BakeryWebSite' do
+  dsc_name 'FourthCoffee'
+  dsc_ensure 'Started'
+  dsc_physicalpath node['fourthcoffee']['install_path']
+end
